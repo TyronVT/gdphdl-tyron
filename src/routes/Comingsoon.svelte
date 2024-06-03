a497d485e964b404b5efa268048827e0d64005e0
TyronVT <tyrontalusan00@gmail.com>
2024-06-03 17:28:24 +0800

    Changed styling and coming soon text

--- src/routes/Comingsoon.svelte
+++ src/routes/Comingsoon.svelte
@@ -18,8 +18,9 @@
     <p>Currently Working Features:</p>
     <a href="/levels"><p>Top 150 List</p></a>
     <a href="/leaderboards"><p>Leaderboards</p></a>
+    <p> Will add mobile support soon, this site only looks good on a desktop. </p>
     <br><br><br>
-    <h1>Coming Soon!</h1>
+    <h1>Other Pages Coming Soon!</h1>
     <h3>(After I finish my Thesis)</h3>
     <h3>-Tyron</h3>
 
